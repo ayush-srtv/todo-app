b241f209b28e28e1a8b1fa8ef88f53ba66ecf29c
Ayush Srivastava <ayushs.1992@gmail.com>
2019-12-31 21:20:37 +0530

    Added icon for no list

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MUIList from "@material-ui/core/List";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import ListAlt from "@material-ui/icons/ListAlt";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
@@ -18,7 +19,14 @@ function List({ children, paperClassName = "" }) {
   return (
     <Paper elevation={3} className={paperClassName}>
       <MUIList className={classes.root}>
-        {children.length === 0 ? <Typography>NO ITEMS</Typography> : children}
+        {children.length === 0 ? (
+          <Typography>
+            <ListAlt />
+            No Items
+          </Typography>
+        ) : (
+          children
+        )}
       </MUIList>
     </Paper>
   );

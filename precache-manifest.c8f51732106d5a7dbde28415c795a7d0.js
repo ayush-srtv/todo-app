8d57981fe819a52ce88c4fea34429b9185b3e70d
Ayush Srivastava <ayushs.1992@gmail.com>
2020-10-12 22:07:09 +0530

    Updates

---
+++ precache-manifest.c8f51732106d5a7dbde28415c795a7d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c747feb036bc0461af41ff27115384",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "60405fcd127c62b0b65f",
+    "url": "/todo-app/static/css/main.1d6dc814.chunk.css"
+  },
+  {
+    "revision": "3b87a1aba0c310e77f81",
+    "url": "/todo-app/static/js/2.e2e63fc9.chunk.js"
+  },
+  {
+    "revision": "60405fcd127c62b0b65f",
+    "url": "/todo-app/static/js/main.2ab920c5.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  }
+]);
